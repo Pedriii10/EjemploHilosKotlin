80c53e08588e7fec0f802a9d05a4aa0106b40beb
Pedriii10 <pedromonzon239@gmail.com>
2023-11-13 10:56:07 +0100

    Ejercicio 2 completo, hilos en kotlin

--- app/src/main/java/com/example/ejemplohiloskotlin/MainActivity.kt
+++ app/src/main/java/com/example/ejemplohiloskotlin/MainActivity.kt
@@ -2,41 +2,102 @@ package com.example.ejemplohiloskotlin
 
 import android.os.AsyncTask
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 
-// Asyntask
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : ComponentActivity() {
+    private lateinit var tvCrono: TextView
+    private lateinit var startButton: Button
+    private lateinit var stopButton: Button
+    private lateinit var restartButton: Button
+    private var countDownTask: CountDownTask? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvCrono: TextView = findViewById(R.id.tvCrono)
+        tvCrono = findViewById(R.id.tvCrono)
+        startButton = findViewById(R.id.startButton)
+        stopButton = findViewById(R.id.stopButton)
+        restartButton = findViewById(R.id.restartButton)
 
-        val asyncTask = MyAsyncTask(tvCrono)
-        asyncTask.execute(10)
+        startButton.setOnClickListener { startOrResumeTask() }
+        stopButton.setOnClickListener { pauseTask() }
+        restartButton.setOnClickListener { restartTask() }
     }
 
-    private class MyAsyncTask(private val textView: TextView) :
-        AsyncTask<Int, Int, String>() {
+    private fun startOrResumeTask() {
+        if (countDownTask == null || countDownTask?.status == AsyncTask.Status.FINISHED) {
+            countDownTask = CountDownTask()
+            countDownTask?.execute(10)
+        } else {
+            countDownTask?.resumeCounting()
+        }
+    }
+
+    private fun pauseTask() {
+        if (countDownTask?.status == AsyncTask.Status.RUNNING) {
+            countDownTask?.pauseCounting()
+        }
+    }
+
+    private fun restartTask() {
+        countDownTask?.cancel(true)
+        countDownTask = CountDownTask()
+        countDownTask?.execute(10)
+        countDownTask?.resumeCounting()
+    }
+
+    private inner class CountDownTask : AsyncTask<Int, Int, String>() {
+
+        private var countingPaused = false
+        private val pauseLock = Object()
 
         override fun doInBackground(vararg params: Int?): String {
             var remaining = params[0] ?: 0
-            while (remaining > 0) {
+            while (remaining > 0 && !isCancelled) {
+                synchronized(pauseLock) {
+                    while (countingPaused) {
+                        try {
+                            pauseLock.wait()
+                        } catch (e: InterruptedException) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
                 publishProgress(remaining)
-                Thread.sleep(1_000)
                 remaining--
+
+                try {
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
             }
             return "Terminado"
         }
 
         override fun onProgressUpdate(vararg values: Int?) {
-            textView.text = values[0].toString()
+            super.onProgressUpdate(*values)
+            tvCrono.text = values[0].toString()
         }
 
         override fun onPostExecute(result: String?) {
-            textView.text = result
+            super.onPostExecute(result)
+            tvCrono.text = result
+        }
+
+        fun pauseCounting() {
+            countingPaused = true
+        }
+
+        fun resumeCounting() {
+            countingPaused = false
+            synchronized(pauseLock) {
+                pauseLock.notifyAll()
+            }
         }
     }
 }
