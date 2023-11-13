978596f42ad48352193b0c3774fabbac9a3c1d9c
Pedriii10 <pedromonzon239@gmail.com>
2023-11-13 10:37:13 +0100

    runOnUiThread

--- app/src/main/java/com/example/ejemplohiloskotlin/MainActivity.kt
+++ app/src/main/java/com/example/ejemplohiloskotlin/MainActivity.kt
@@ -4,19 +4,29 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
-//Ocurre lo mismo que en java, la app no carga hasta que el hilo termina
+
+// Segundo Hilo con runOnUiThread
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val tvCrono: TextView = findViewById(R.id.tvCrono)
-        var remaining = 10
-        while (remaining > 0) {
-            tvCrono.text = remaining.toString()
-            Thread.sleep(1_000)
-            remaining--
-        }
-        tvCrono.text = "Terminado"
+
+        Thread {
+            var remaining = 10
+            while (remaining > 0) {
+                runOnUiThread {
+                    tvCrono.text = remaining.toString()
+                }
+                Thread.sleep(1_000)
+                remaining--
+            }
+
+            tvCrono.post {
+                tvCrono.text = "Terminado"
+            }
+        }.start()
     }
 }
